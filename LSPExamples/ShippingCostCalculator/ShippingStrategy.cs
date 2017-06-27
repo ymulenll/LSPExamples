be1fda8019fb54ba097125c030bc99f10238fce2
Yoelvis Mulen <ymulenll@hotmail.com>
2017-06-27 12:01:17 -0300

    added code contracts to base class.

--- LSPExamples/ShippingCostCalculator/ShippingStrategy.cs
+++ LSPExamples/ShippingCostCalculator/ShippingStrategy.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿#define CONTRACTS_FULL
+
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -23,29 +26,27 @@ namespace ShippingCostCalculator
             }
             set
             {
-                // Invariants.
-                if (value <= decimal.Zero)
-                    throw new ArgumentOutOfRangeException("flatRate", "Flat rate must be positive and non-zero");
-
                 this.flatRate = value;
             }
+        }
+
+        [ContractInvariantMethod]
+        private void ClassInvariant()
+        {
+            Contract.Invariant(this.FlatRate > decimal.Zero, "Flat rate must be positive and non-zero");
         }
 
         public virtual decimal CalculateShippingCost(
             float packageWeightInKilograms, 
             RegionInfo destination)
         {
-            // Preconditions.
-            if (packageWeightInKilograms <= 0)
-                throw new ArgumentOutOfRangeException(nameof(packageWeightInKilograms), "Package weight must be positive and non-zero");
+            // Preconditions and postconditions.
+            Contract.Requires<ArgumentOutOfRangeException>(packageWeightInKilograms > 0, "Package weight must be positive and non-zero");
+            Contract.Ensures(Contract.Result<decimal>() > decimal.Zero, "The return value is out of range");
 
             // implementation
             var shippingCost = decimal.One;
-
-            // Postconditions.
-            if (shippingCost <= decimal.Zero)
-                throw new ArgumentOutOfRangeException("return", "The return value is out of range");
-
+            
             return shippingCost;
         }
     }
